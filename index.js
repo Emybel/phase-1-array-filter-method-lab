5f83562c011f76adc310bf0638eaacd0aa042042
Imane Belaid <belaidimane@gmail.com>
2023-08-07 22:55:29 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+function findMatching(array,name){
+    const driverNames = array.filter((element) =>{
+        if(element.toLowerCase() === name.toLowerCase()){
+            return element
+        }
+    });
+    return driverNames
+}
+function fuzzyMatch(array,letter){
+    const driverNames = array.filter((element) =>{
+        if(element.startsWith(letter)){
+            return element
+        }
+    })
+    return driverNames
+}
+
+function matchName(array,Name) {
+    const driverNames = array.filter((element) =>{
+        if (element.name === Name){
+            return element
+        }
+    })
+    return driverNames
+}
